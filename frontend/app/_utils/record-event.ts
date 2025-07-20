fbef23fa92b1e002fbdb7ce7e231abdaa0caa9ab
Rohit <rohit.mmm1996@gmail.com>
2025-07-21 04:09:24 +0530

    refactor: simplify event tracking logic in recordEvent function by consolidating window checks and removing unnecessary console log

--- frontend/app/_utils/record-event.ts
+++ frontend/app/_utils/record-event.ts
@@ -1,8 +1,6 @@
 export const recordEvent = (event: string, data: Record<string, unknown>) => {
-    const unami = typeof window !== "undefined" ? (window as unknown as { unami: { track: (event: string, data: Record<string, unknown>) => void } }) : undefined;
-    if (unami) {
-        unami.unami.track?.(event, data);
-    } else {
-        console.log("Unami not found");
+    if (typeof window !== "undefined") {
+        (window as unknown as { unami: { track: (event: string, data: Record<string, unknown>) => void } })
+            ?.unami?.track?.(event, data);
     }
 };
