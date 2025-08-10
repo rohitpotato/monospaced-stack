b606acbed40fee1f546c1cbad94a846a4cc5933c
rohitpotato <rohit.mmm1996@gmail.com>
2025-08-10 22:34:00 +0530

    chore: remove MDX configuration from Next.js setup to simplify the configuration file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,5 @@
-import createMDX from '@next/mdx';
 import type { NextConfig } from "next";
 
-const withMDX = createMDX({
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -33,4 +26,4 @@ const nextConfig: NextConfig = {
   }
 };
 
-export default withMDX(nextConfig);
+export default (nextConfig);
