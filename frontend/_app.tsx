f2f03ab2bab9410166bfd77975474c0b3efacc33
rohitpotato <rohit.mmm1996@gmail.com>
2025-10-19 03:00:20 +0530

    refactor: remove click sound functionality from the app component to streamline user interactions

--- frontend/_app.tsx
+++ frontend/_app.tsx
@@ -3,28 +3,6 @@
 import { useEffect, useRef } from 'react'
 import MonitorBoi from '@/monitor-boi'
 
-function playClickSound() {
-  try {
-    const audioContext = new (window.AudioContext || (window as any).webkitAudioContext)()
-    const oscillator = audioContext.createOscillator()
-    const gainNode = audioContext.createGain()
-
-    oscillator.connect(gainNode)
-    gainNode.connect(audioContext.destination)
-
-    oscillator.type = 'square'
-    oscillator.frequency.setValueAtTime(1000, audioContext.currentTime)
-    gainNode.gain.setValueAtTime(0.05, audioContext.currentTime)
-
-    gainNode.gain.exponentialRampToValueAtTime(0.00001, audioContext.currentTime + 0.1)
-    oscillator.start(audioContext.currentTime)
-    oscillator.stop(audioContext.currentTime + 0.1)
-  }
-  catch (e) {
-    console.error('Could not play sound:', e)
-  }
-}
-
 const monitorBoi = new MonitorBoi({
   // eslint-disable-next-line node/prefer-global/process
   endpoint: process.env.NEXT_PUBLIC_METRICS_ENDPOINT || '',
@@ -45,19 +23,6 @@ function App({ children }: { children: React.ReactNode }) {
     init()
   }, [])
 
-  useEffect(() => {
-    const handleClick = (event: MouseEvent) => {
-      const target = event.target as HTMLElement
-      if (target.closest('a, button')) {
-        playClickSound()
-      }
-    }
-
-    document.addEventListener('click', handleClick)
-    return () => {
-      document.removeEventListener('click', handleClick)
-    }
-  }, [])
   return children
 }
 
